99fe14daa074a32a1fd36997532e2b0d965de735
Gabriel Santana <gabrielsantana77@hotmail.com>
2020-05-28 06:31:32 -0300

    Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticate from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +28,16 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticate,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
