8e79a45b5b95cd19dbb008488e4e5f6ecee7ad17
Gabriel Santana <gabrielsantana77@hotmail.com>
2020-06-16 14:35:18 -0300

    Criando classe de erro

--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app.use(routes);
 app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('Server started on port 3333!');
 });
