d6cac88d49056ccc6980c5fce72e6f1cfeca1188
Gabriel Santana <gabrielsantana77@hotmail.com>
2020-11-17 18:02:59 -0300

    tests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -37,5 +37,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log('Server started on port 3333! ✔');
 });
